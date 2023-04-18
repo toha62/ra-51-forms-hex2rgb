056d25870b3042262cd2107f906be73a4d6a2dd4
Anton Trifonov <toha62@inbox.ru>
2023-04-18 12:19:00 +0300

    complete

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
 
 export default function Converter() {
-  const [value, setValue] = useState('');
+  const [state, setState] = useState({rgbColor: '', color: '#cccccc'});
 
   const handleChange = ({ target: {value} }) => {
     if (value.length < 7) {
-      setValue('');
+      setState({rgbColor: '', color: '#cccccc'});
       return null;
     }
-    if (!value.match(/#[a-f\d]{6}/i)) {      
-      setValue('ОШИБКА !');
+
+    const hex = value.match(/#([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})/i);
+  
+    if (!hex) {      
+      setState({rgbColor: 'ОШИБКА!', color: 'red'});
       return null;
     }
-    setValue(parseInt(value.slice(1), 16));
-    console.log(value.slice(1));
+
+    setState({
+      rgbColor: `rgb(${parseInt(hex[1], 16)}, ${parseInt(hex[2], 16)}, ${parseInt(hex[3], 16)})`,
+      color: value,
+    });    
   }
 
   return (
-    <div className="container">
+    <div className="container" style={{backgroundColor: state.color}}>
       <form action="">
         <input
           className="user-input box"
           onChange={handleChange}
           type="text"
           maxLength="7"
-          placeholder="Введите код цвета в формате HEX"
+          placeholder="код цвета в формате HEX"
         />
-        <input className="result box" type="text" value={value} readOnly />
+        <input className="result box" type="text" value={state.rgbColor} readOnly />
       </form>
     </div>
   );
